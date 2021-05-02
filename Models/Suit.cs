6afb76fce3d916a3356c1f0481530ebbe646fbd5
ArisAgnew <aris.sektor@gmail.com>
2021-05-03 01:50:15 +0300

    Create Suit.cs

---
+++ Models/Suit.cs
@@ -0,0 +1,12 @@
+﻿using MarvellousPoker.Controllers.Adjuvants;
+
+namespace MarvellousPoker.Models
+{
+    public enum Suit : ushort
+    {
+        [Description("\u2666")] DIAMOND = 1,
+        [Description("\u2665")] HEART = 2,
+        [Description("\u2663")] CLUB = 3,
+        [Description("\u2660")] SPADE = 4
+    }
+}
