87741e443c270b56fc9027d743f9f49d7e04b3f3
ArisAgnew <aris.sektor@gmail.com>
2021-05-03 13:18:32 +0300

    Create EventHandlerExtentions.cs

---
+++ Controllers/Adjuvants/EventHandlerExtentions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MarvellousPoker.Controllers.Adjuvants
+{
+    public static class EventHandlerExtentions
+    {
+        public static void SafeRaise(this EventHandler handler, object sender, EventArgs e) =>
+            handler?.Invoke(sender, e);
+
+        public static void SafeRaise<TEventArgs>(this EventHandler<TEventArgs> handler, object sender, TEventArgs e)
+            where TEventArgs : EventArgs => handler?.Invoke(sender, e);
+    }
+}
