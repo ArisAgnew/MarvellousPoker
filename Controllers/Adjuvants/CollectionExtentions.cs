a8de5faef67d09a160b7b6d4e337ee86a1fdc973
ArisAgnew <aris.sektor@gmail.com>
2021-05-05 21:14:09 +0300

    Create CollectionExtentions.cs

---
+++ Controllers/Adjuvants/CollectionExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MarvellousPoker.Controllers.Adjuvants
+{
+    public static class CollectionExtentions
+    {
+        public static ICollection<T> OughtToBeEmpty<T>(this ICollection<T> collection)
+            where T : notnull
+        {
+            if (collection.Any() && collection is not null)
+            {
+                collection.Clear();
+            }
+            return collection;
+        }
+    }
+}
