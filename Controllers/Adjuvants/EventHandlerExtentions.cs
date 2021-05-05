e2f5ea906ae858f14ef2083a2fe596e9d5c782e1
ArisAgnew <aris.sektor@gmail.com>
2021-05-05 21:14:15 +0300

    Update EventHandlerExtentions.cs

--- Controllers/Adjuvants/EventHandlerExtentions.cs
+++ Controllers/Adjuvants/EventHandlerExtentions.cs
@@ -4,10 +4,10 @@ namespace MarvellousPoker.Controllers.Adjuvants
 {
     public static class EventHandlerExtentions
     {
-        public static void SafeRaise(this EventHandler handler, object sender, EventArgs e) =>
+        public static void SafeEvoke(this EventHandler handler, object sender, EventArgs e) =>
             handler?.Invoke(sender, e);
 
-        public static void SafeRaise<TEventArgs>(this EventHandler<TEventArgs> handler, object sender, TEventArgs e)
-            where TEventArgs : EventArgs => handler?.Invoke(sender, e);
+        public static void SafeEvoke<TEventArgs>(this EventHandler<TEventArgs> handler, object sender, TEventArgs e)
+            where TEventArgs : class => handler?.Invoke(sender, e);
     }
 }
