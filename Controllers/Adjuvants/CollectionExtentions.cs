edc0b27e6117b65c8c572d303f2e35990b5c1e0e
ArisAgnew <aris.sektor@gmail.com>
2021-05-08 21:38:52 +0300

    Update CollectionExtentions.cs

--- Controllers/Adjuvants/CollectionExtentions.cs
+++ Controllers/Adjuvants/CollectionExtentions.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace MarvellousPoker.Controllers.Adjuvants
 {
-    public static class CollectionExtentions
+    internal static class CollectionExtentions
     {
-        public static ICollection<T> OughtToBeEmpty<T>(this ICollection<T> collection)
+        internal static ICollection<T> OughtToBeEmpty<T>(this ICollection<T> collection)
             where T : notnull
         {
             if (collection.Any() && collection is not null)
