b16e493130c90e85c367e717a31dc20d8ac1432d
ArisAgnew <aris.sektor@gmail.com>
2021-05-08 21:39:41 +0300

    Update HashExtentions.cs

--- Controllers/Adjuvants/HashExtentions.cs
+++ Controllers/Adjuvants/HashExtentions.cs
@@ -4,33 +4,33 @@ using static System.Int32;
 
 namespace MarvellousPoker.Controllers.Adjuvants
 {
-    public static class HashExtentions
+    internal static class HashExtentions
     {
-        private static readonly int baseValue = new Random().Next(MinValue, MaxValue) >> 5;
+        private static readonly int BASE_VALUE = new Random().Next(MinValue, MaxValue) >> 5;
 
-        public static int GetHashCode<T>(this T arg)
+        internal static int GetHashCode<T>(this T arg)
             where T : notnull
-            => unchecked(baseValue + arg.GetHashCode());
+            => unchecked(BASE_VALUE + arg.GetHashCode());
 
-        public static int GetHashCode<T1, T2>(this T1 arg1, T2 arg2)
+        internal static int GetHashCode<T1, T2>(this T1 arg1, T2 arg2)
             where T1 : notnull where T2 : notnull
             => unchecked(arg1.GetHashCode<T1>() +
                 arg2.GetHashCode<T2>());
 
-        public static int GetHashCode<T1, T2, T3>(this T1 arg1, T2 arg2, T3 arg3)
+        internal static int GetHashCode<T1, T2, T3>(this T1 arg1, T2 arg2, T3 arg3)
             where T1 : notnull where T2 : notnull where T3 : notnull
             => unchecked(arg1.GetHashCode<T1>() +
                 arg2.GetHashCode<T2>() +
                 arg3.GetHashCode<T3>());
 
-        public static int GetHashCode<T1, T2, T3, T4>(this T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        internal static int GetHashCode<T1, T2, T3, T4>(this T1 arg1, T2 arg2, T3 arg3, T4 arg4)
             where T1 : notnull where T2 : notnull where T3 : notnull where T4 : notnull
             => unchecked(arg1.GetHashCode<T1>() +
                 arg2.GetHashCode<T2>() +
                 arg3.GetHashCode<T3>() +
                 arg4.GetHashCode<T4>());
 
-        public static int GetHashCode<T1, T2, T3, T4, T5>(this T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        internal static int GetHashCode<T1, T2, T3, T4, T5>(this T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
             where T1 : notnull where T2 : notnull where T3 : notnull where T4 : notnull where T5 : notnull
             => unchecked(arg1.GetHashCode<T1>() +
                 arg2.GetHashCode<T2>() +
