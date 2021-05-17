f05995f586394772813d1b69491a9d4787cd2965
ArisAgnew <aris.sektor@gmail.com>
2021-05-17 23:07:58 +0300

    Create PotentialNullValueExtentions.cs

---
+++ Controllers/Adjuvants/PotentialNullValueExtentions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static System.String;
+
+namespace MarvellousPoker.Controllers.Adjuvants
+{
+    internal static class PotentialNullValueExtentions
+    {
+        private static readonly string EXCEPTION_MESSAGE = "{0} has null value.";
+
+        internal static T MightBeNullOrDefault<T>(this T adjacentType, string message = default) =>
+            adjacentType switch
+            {
+                T when EqualityComparer<T>.Default.Equals(adjacentType, default) => default,
+                null => throw new ArgumentNullException(Format(EXCEPTION_MESSAGE, message)),
+                not null => adjacentType
+            };
+
+        internal static T MightBeNullOrDefault<T>(this T adjacentType,
+                                                  Func<T, T> func,
+                                                  string message) => func.MightBeNullOrDefault(message)(adjacentType);
+    }
+}
