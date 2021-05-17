bdf2e503696e9593bdee7d8f668a376d0d2c7809
ArisAgnew <aris.sektor@gmail.com>
2021-05-17 14:50:15 +0300

    Create GameGeneralException.cs

---
+++ Meta/CustomExceptions/GameGeneralException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MarvellousPoker.Meta.CustomExceptions
+{
+    /*
+     * При обработке исключения, производного от System.ApplicationException, можно смело предполагать, 
+     * что исключение было инициировано кодом работающего приложения, а не сторонней библиотекой.
+     */
+    [Serializable]
+    internal class GameGeneralException : ApplicationException
+    {
+        private string _gameExceptionMessage;
+
+        public string GameExceptionMessage
+        {
+            get => _gameExceptionMessage;
+            set
+            {
+                if (_gameExceptionMessage != value)
+                {
+                    _gameExceptionMessage = value;
+                }
+            }
+        }
+
+        public GameGeneralException()
+        {
+        }
+
+        public GameGeneralException(string message) : base(message)
+        {
+        }
+
+        public GameGeneralException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected GameGeneralException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            _gameExceptionMessage = info?.GetString(nameof(GameExceptionMessage));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue(nameof(GameExceptionMessage), GameExceptionMessage);
+        }
+    }
+}
