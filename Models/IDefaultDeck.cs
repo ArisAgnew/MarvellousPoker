a405ee16c737661022d5e9061843446677a5ff7f
ArisAgnew <aris.sektor@gmail.com>
2021-05-17 03:43:48 +0300

    Create IDefaultDeck.cs

---
+++ Models/IDefaultDeck.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MarvellousPoker.Models
+{
+    public interface IDefaultDeck<out T>
+    {
+        IReadOnlyCollection<T> GetBrandNewDeck();
+    }
+}
