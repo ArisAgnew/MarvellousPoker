409fd32143c916accc1f97bec3e9a7a5b383db2d
ArisAgnew <aris.sektor@gmail.com>
2021-06-27 01:51:19 +0300

    Create CacheExtentionsTrial.cs

---
+++ Controllers/Adjuvants/CacheExtentionsTrial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using static System.DateTime;
+
+namespace MarvellousPoker.Controllers.Adjuvants
+{
+    public static class CacheExtentionsTrial
+    {
+        public static Func<T> TimeCache<T>(this Func<T> func, int cacheInterval)
+        {
+            var cachedValue = func();
+            var timeCached = Now;
+
+            return () =>
+            {
+                if ((Now - timeCached).Seconds >= cacheInterval)
+                {
+                    timeCached = Now;
+                    cachedValue = func();
+                }
+                return cachedValue;
+            };
+        }
+    }
+}
