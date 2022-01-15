90c87455cdf8619b31973d5fa11321589a7dd44e
ArisAgnew <aris.sektor@gmail.com>
2022-01-15 12:16:11 +0300

    Create Namespaces.cs

---
+++ Namespaces.cs
@@ -0,0 +1,23 @@
+﻿#region global general namespaces
+global using System;
+global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
+global using System.Diagnostics;
+global using System.Collections.Immutable;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Linq;
+global using System.Reflection;
+global using System.Runtime.Serialization;
+global using MarvellousPoker.Controllers.Adjuvants;
+global using MarvellousPoker.Models;
+global using MarvellousPoker.Models.Abstractions;
+global using MarvellousPoker.Controllers.Core;
+#endregion global general namespaces
+
+#region global auxiliary namespaces
+global using static System.Console;
+global using static System.ConsoleColor;
+global using static System.DateTime;
+global using static System.String;
+global using static System.HashCode;
+#endregion global auxiliary namespaces
