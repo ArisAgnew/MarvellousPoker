d9af7250d4454bdb3e3d455a28c46abc2f428c53
ArisAgnew <aris.sektor@gmail.com>
2022-01-15 11:32:12 +0300

    Update Rank.cs

--- Models/Rank.cs
+++ Models/Rank.cs
@@ -1,26 +1,22 @@
-﻿using System;
-
-using MarvellousPoker.Controllers.Adjuvants;
-
-namespace MarvellousPoker.Models
+﻿namespace MarvellousPoker.Models
 {
     [Flags]
     public enum Rank : ushort
     {
-        [Description("\x32")] TWO = 0b_0000_0010,
-        [Description("\x33")] THREE = 0b_0000_0011,
-        [Description("\x34")] FOUR = 0b_0000_0100,
-        [Description("\x35")] FIVE = 0b_0000_0101,
-        [Description("\x36")] SIX = 0b_0000_0110,
-        [Description("\x37")] SEVEN = 0b_0000_0111,
-        [Description("\x38")] EIGHT = 0b_0000_1000,
-        [Description("\x39")] NINE = 0b_0000_1001,
-        [Description("10")] TEN = 0b_0000_1010,
+        [Description("\x32")] TWO = 0x02,
+        [Description("\x33")] THREE = 0x03,
+        [Description("\x34")] FOUR = 0x04,
+        [Description("\x35")] FIVE = 0x05,
+        [Description("\x36")] SIX = 0x06,
+        [Description("\x37")] SEVEN = 0x07,
+        [Description("\x38")] EIGHT = 0x08,
+        [Description("\x39")] NINE = 0x09,
+        [Description("10")] TEN = 0x0A,
 
-        [Description("\x4A")] JACK = 0b_0001_0000,
-        [Description("\x51")] QUEEN = 0b_0010_0000,
-        [Description("\x4B")] KING = 0b_0100_0000,
-        [Description("\x41")] ACE = 0b_1000_0000,
-        //[Description("\x74")] THE_JOKER = 0b_100_0000_0000,
+        [Description("\x4A")] JACK = 0x0B,
+        [Description("\x51")] QUEEN = 0x0C,
+        [Description("\x4B")] KING = 0x0D,
+        [Description("\x41")] ACE = 0x0E,
+        //[Description("\x74")] THE_JOKER = 0x0F,
     }
 }
